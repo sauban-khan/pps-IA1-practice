e4cd7ef4252cabf440d686cc20169fec78046114
sauban-khan <95756161+sauban-khan@users.noreply.github.com>
2022-01-31 05:45:37 +0000

    31st jan 2022

--- p1original.c
+++ p1original.c
@@ -1,2 +1,11 @@
-
+#include <stdio.h>
+int main()
+{
+  printf("Start Of The Program\n ");
+  int a,b,sum;
+  printf("Enter two numbers\n");
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  printf("%d+%d=%d\n",a,b,sum);
+  return 0;
 }
