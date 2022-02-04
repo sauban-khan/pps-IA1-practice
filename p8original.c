1b4c7e9e86823e855ce4c5a0f07dc1d970fa75d2
sauban-khan <95756161+sauban-khan@users.noreply.github.com>
2022-02-04 09:25:21 +0000

    4th feb 2022

--- p8original.c
+++ p8original.c
@@ -1,35 +1,57 @@
 #include<stdio.h>
-
-float input()
+typedef struct _complex
 {
-  float n;
-  printf("Enter a number to find the sqaure root of: ");
-  scanf("%f", &n);
+  float real;
+  float imaginary;
+}Complex;
+
+int get_n(){
+  int n;
+  printf("Enter the number of complex numbers: ");
+  scanf("%d", &n);
   return n;
 }
 
-float squareroot(float n)
-{
-  float x = n;
-  float y = 1;
-  float e = 0.000001;
-  while (x - y > e)
-  {
-    x  = (x + y) / 2;
-    y = n / x; 
+void input_n_complex(int n, Complex c[n]){
+  for(int i=0; i<n; i++){
+    printf("Enter the real part: ");
+    scanf("%f", &c[i].real);
+    printf("Enter the imaginary part: ");
+    scanf("%f", &c[i].imaginary);
   }
-  return x;
 }
-void output(float n, float sres)
-{
-  printf("Square root of given input %0.2f is = %0.2f \n", n, sres);
+
+Complex add_n_complex(int n, Complex c[n]){
+  Complex res;
+  res.real = 0;
+  res.imaginary = 0;
+  for(int i=0; i<n; i++){
+    res.real += c[i].real; 
+    res.imaginary += c[i].imaginary;
+  }
+  return res;
+}
+
+void output(int n, Complex c[n], Complex result){
+  if(n==1){
+      printf("%0.2f + %0.2fi is %0.2f + %0.2fi\n", c[0].real, c[0].imaginary, result.real, result.imaginary);
+  }else{
+      for(int i=0; i<n-1; i++){
+        printf("%0.2f + %0.2fi\n", c[i].real, c[i].imaginary);
+      }
+      printf("+ %0.2f + %0.2fi is\n%0.2f + %0.2fi\n", c[n-1].real, c[n-1].imaginary, result.real, result.imaginary);
+  }
+
+
 }
 
 int main()
 {
-  float n, sres;
-  n = input();
-  sres = squareroot(n);
-  output(n, sres);
+  printf("Start Of The Program:\n");
+  int n = get_n();
+  Complex c[n], res;
+  input_n_complex(n, c);
+  res = add_n_complex(n, c);
+  output(n, c, res);
   return 0;
-} 
+}
