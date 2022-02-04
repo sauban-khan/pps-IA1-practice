a44dc971e9cd0ea33f1e2a51c298b91993a709f2
sauban-khan <95756161+sauban-khan@users.noreply.github.com>
2022-02-04 09:19:36 +0000

    4th feb 2022

--- p6original.c
+++ p6original.c
@@ -1,37 +1,40 @@
 #include<stdio.h>
-
-int input()
+void input(char *str1, char *str2)
 {
-    int a;
-    printf("Enter the numbers: ");
-    scanf("%d", &a);
-    return a;
+  printf("Enter a string: ");
+  scanf("%s",str1);
+  printf("Enter a string: ");
+  scanf("%s",str2);
 }
-
-int sum_n(int n)
+int cmp(char *str1, char *str2)
+{ 
+  int i;
+  for(i = 0; str1[i] && (str1[i] == str2[i]); i++)
+  {
+    return str1[i] - str2[i] ;
+  }
+}
+void output(char *str1, char *str2, int res)
 {
-    int ans=0;
-    for(int i=1; i<=n; i++)
-    {
-        ans = ans + i;
-    }
-    return ans;
+  if(res < 0)
+  {
+    printf("%s is greater than %s", str2, str1);
+  }
+  else if(res > 0)
+  {
+    printf("%s is greater than %s", str1, str2);
+  }
+  else
+  {
+    printf("Strings are equal");
+  }
 }
-
-void output(int n, int sum)
-{
-    for(int i=1; i<n; i++)
-    {
-      printf("%d+", i);
-    }
-    printf("%d is %d\n", n, sum);
-}
-
 int main()
 {
-    printf("Start Of The Program:\n");
-    int n = input();
-    int sum = sum_n(n);
-    output(n, sum);
-    return 0;
-}
+  char str1[50], str2[50];
+  int result;
+  input(str1, str2);
+  result = cmp(str1, str2);
+  output(str1, str2, result);
+  return 0;
+}  
